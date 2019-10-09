0534e2c336126631428c4ecc30b93d7aa146a85a
greywall <talwindergrewal01@gmail.com>
2019-10-08 19:48:48 -0700

    Initial Commit

---
+++ server/config/postgres.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+
+module.exports = (app) => {
+  
+  app.get('PG_DB')
+  return new Pool({
+    user: app.get("PG_USER"), 
+    host: app.get("PG_HOST") ,
+    database: app.get("PG_DB"), 
+    password: app.get("PG_PASSWORD"), 
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  });
+};
