201b949784f794f136350b1a91f8a42a469ce44a
greywall <talwindergrewal01@gmail.com>
2019-10-20 13:38:50 -0700

    Add multiple item cards to items page

--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -11,58 +11,60 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
 
 const ItemCard = ({ classes, item }) => {
   return (
-    <div>
-      <Card className={classes.cardholder}>
-        <CardActionArea>
+    <Card className={classes.cardholder}>
+      <CardActionArea>
+        <Link to="http://www.google.com">
           <CardMedia
-            src="http://place-puppy.com/500x280"
             className={classes.cardimage}
             image="http://place-puppy.com/500x280"
-            title="Cute Puppy"
+            title={item.title}
           />
+        </Link>
+        <CardContent>
+          <div className={classes.cardcontentdiv}>
+            <Avatar
+              className={classes.personicon}
+              alt="Puppy Name"
+              src="http://place-puppy.com/500x280"
+            />
+            <Typography className={classes.userinfo}>
+              <Box>{item.itemowner.fullname}</Box>
+              <Box color="lightgrey">25 years ago</Box>
+            </Typography>
+          </div>
 
-          <CardContent>
-            <div className={classes.cardcontentdiv}>
-              <Avatar
-                className={classes.personicon}
-                alt="Puppy Name"
-                src="http://place-puppy.com/500x280"
-              />
-              <Typography className={classes.userinfo}>
-                <Box>Jerry Jefferson</Box>
-                <Box color="lightgrey">25 years ago</Box>
-              </Typography>
-            </div>
+          <div className={classes.carddetails}>
+            <Typography>
+              <Box className={classes.carddetail} className={classes.cardtitle}>
+                {item.title}
+              </Box>
 
-            <div className={classes.carddetails}>
-              <Typography>
+              {item.tags.map(tag => (
                 <Box
+                  color="lightgrey"
                   className={classes.carddetail}
-                  className={classes.cardtitle}
+                  key={tag.id}
                 >
-                  The Name of the card
+                  {tag.title}
                 </Box>
-                <Box color="lightgrey" className={classes.carddetail}>
-                  This is where the tags go This is where the tags go This is
-                  where the tags go This is where the tags go This is where the
-                  tags go This is where the tags go
-                </Box>
-                <Box className={classes.carddetail}>Item Description</Box>
-              </Typography>
-            </div>
-          </CardContent>
-        </CardActionArea>
+              ))}
 
-        <CardActions className={classes.cardbutton}>
-          <Button variant="contained" className={classes.buttonborrow}>
-            Borrow
-          </Button>
-        </CardActions>
-      </Card>
-    </div>
+              <Box className={classes.carddetail}>{item.description}</Box>
+            </Typography>
+          </div>
+        </CardContent>
+      </CardActionArea>
+
+      <CardActions className={classes.cardbutton}>
+        <Button variant="contained" className={classes.buttonborrow}>
+          Borrow
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
--- client/src/components/Itemsgrid/itemsgrid.js
+++ client/src/components/Itemsgrid/itemsgrid.js
@@ -3,15 +3,16 @@ import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import ItemCard from "../ItemCard/ItemCard";
 import styles from "./styles";
-import { mergeClasses } from "@material-ui/styles";
 
 const ItemsGrid = ({ classes, items }) => {
   return (
-    <div>
-      <Grid>
-        <ItemCard className={classes.itemgridholder} />
-      </Grid>
-    </div>
+    <Grid container className={classes.itemsgridholder}>
+      {items.map(item => (
+        <Grid item xs={4} key={item.id}>
+          <ItemCard className={classes.itemgridholder} item={item} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
