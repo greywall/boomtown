277d74d98c97acee2499727b264b947bac4c8695
greywall <talwindergrewal01@gmail.com>
2019-11-27 10:44:13 -0800

    Add Profile Page Data

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,13 +1,43 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
+import { Typography, CardHeader, Avatar } from "@material-ui/core";
+import Gravatar from "react-gravatar";
+import ItemsGrid from "../../components/ItemsGrid";
 import styles from "./styles";
+import PropTypes from "prop-types";
 
-const Profile = ({ classes }) => {
+const Profile = ({ classes, user }) => {
   return (
-    <div>
-      <p>
-        This is the profile page located at <code>/profile/:userId</code>.
-      </p>
+    <div className={classes.root}>
+      {console.log(user)}
+
+      <div className={classes.container}>
+        <CardHeader
+          className={classes.header}
+          classes={{ title: classes.title }}
+          title={user.fullname}
+          avatar={
+            <Avatar round="true" className={classes.avatar}>
+              <Gravatar email={user.email} />
+            </Avatar>
+          }
+        />
+        <div className={classes.info}>
+          <Typography>
+            {user.items.length} Items shared {user.borrowed.length} Items
+            borrowed
+          </Typography>
+
+          {user.bio === null ? (
+            <Typography className={classes.bio}>"No bio provided"</Typography>
+          ) : (
+            <Typography className={classes.bio}>{user.bio}</Typography>
+          )}
+        </div>
+      </div>
+      <div className={classes.items}>
+        {user.items && <ItemsGrid items={user.items} />}
+      </div>
     </div>
   );
 };
--- client/src/pages/Profile/ProfileContainer.js
+++ client/src/pages/Profile/ProfileContainer.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 import Profile from "./Profile";
-// import FullScreenLoader from '../../components/FullScreenLoader';
-// import { Query } from 'react-apollo';
-// import {  } from '../../apollo/queries';
+import FullScreenLoader from "../../components/FullScreenLoader";
+import { Query } from "react-apollo";
+import { ALL_USER_ITEMS_QUERY } from "../../apollo/queries";
+import { ViewerContext } from "../../context/ViewerProvider";
 
 class ProfileContainer extends Component {
   render() {
-    return <Profile />;
+    return (
+      <ViewerContext.Consumer>
+        {({ viewer }) => (
+          <div>
+            <Query
+              query={ALL_USER_ITEMS_QUERY}
+              variables={{ id: this.props.match.params.userId || viewer.id }}
+            >
+              {({ loading, error, data }) => {
+                if (loading) return "Loading...";
+                if (error) return `Error! ${error.message}`;
+                return <Profile user={data.user} />;
+              }}
+            </Query>
+          </div>
+        )}
+      </ViewerContext.Consumer>
+    );
   }
 }
 
